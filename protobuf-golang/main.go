c94698a6a0ddfce664f5edd2d8411fcb7df8fe68
Elliot Moule <ellimyt@live.com.au>
2019-10-18 16:06:55 +1100

    [began golang protobuf project]

---
+++ protobuf-golang/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	protocol_buffers/src/example"
+)
+
+func main() {
+	fmt.Println("Hello World")
+
+	doSimple()
+}
+
+func doSimple() {
+	example_simple.SimpleMessage
+}
